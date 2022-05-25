052954d9c881c3b448ca2ed26f4f851deed44945
JORGE <jcgarciamed@gmail.com>
2022-05-25 10:49:41 -0500

    Adding Beer Validation

--- src/main/java/com/greymatter/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/greymatter/msscbeerservice/web/model/BeerDto.java
@@ -4,8 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.util.pattern.PathPattern;
 
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,13 +19,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
+
+
     private Integer quantityOnHand;
 }
